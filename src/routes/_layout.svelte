7b54cc257fad48a35c48890f793baf1b4b049584
atav1k <atavikram@gmail.com>
2020-04-07 17:36:18 -0600

    postnote init

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,30 @@
 <script>
 	import Nav from '../components/Nav.svelte';
 
-	export let segment;
+	// export let segment;
 </script>
 
 <style>
 	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
+		max-width: 62rem;
 		margin: 0 auto;
-		box-sizing: border-box;
 	}
 </style>
 
-<Nav {segment}/>
+<!-- <Nav {segment}/> -->
 
-<main>
-	<slot></slot>
+<main p="10 sm3">
+
+  <section py="4">
+    <slot />
+  </section>
+
+  <footer>
+    <nav>
+      <div>
+        <a href="https://github.com/atav1k/">Github</a> / 
+        <a href="/">Postnote</a> &copy; {new Date().getFullYear()}
+      </div>
+    </nav>
+  </footer>
 </main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,42 @@
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+[list]{
+	border-left: 1px solid var(--gray-6);
+}
 </style>
 
 <svelte:head>
 	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Postnote</h1>
+<h2>Sharing Is Sense-making.</h2>
+<p>Postnote is bookmarking and knowledge sharing for teams. We help your team manage insights and ideas: list, categorize, reply, relate, and share.</p>
+<p>Try postnote by visiting our team's bookmark repository.</p>
 
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<ul mt="8" grid columns="2" gap="2">
+	<li cell>
+		<h3>Post</h3>
+		<p>More than notes. Connect ideas in new ways.</p>
+	</li>
+	<li cell>
+		<ul list p="4" bg="gray2" mb="6">
+			<li>Topics: Include 1-3 topics with each note.</li>
+			<li>Lists: Add notes to public or private lists.</li>
+			<li>Relationships: Reference any other $note.</li>
+			<li>Scrape: Pull draft notes from feeds.</li>
+		</ul>
+	</li>
+	<li cell>
+		<h3>Note</h3>
+		<p>A simple reference. No vanity information.</p>
+	</li>
+	<li cell>		
+		<ul list p="4" bg="gray2" mb="6">
+			<li>Notes: The plaintext link and post.</li>
+			<li>Replies: Plaintext threads.</li>
+			<li>Types: Support for email and messages.</li>
+			<li>Reactions: Respond by emoji.</li>
+			<li>Log: Track changes.</li>
+		</ul>
+	</li>
+</ul>
